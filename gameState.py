7cf616f481ed586572ebc00a7c1466ada165c828
Oskar Marszałek <oskarm91@o2.pl>
2014-10-15 20:52:02 +0200

    initial

---
+++ gameState.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+class GameState:
+    pass
+
+GameState.end_game = False
+GameState.start_screen = True
+GameState.vector = 0
+GameState.shoot_bullet = False
---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+class Game(object):
+    def __init__(self):
+        pygame.init()
+
+
+
+if __name__ == '__main__':
+    pv = Game()
+    pv.main_loop()
